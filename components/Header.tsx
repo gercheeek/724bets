77a4c852bea6eeae464e86589b4e10e834b68271
alex <banabirsehiranlatbaba@gmail.com>
2026-07-06 15:13:07 +0300

    Fix Header onToggleSidebar reference crash

--- components/Header.tsx
+++ components/Header.tsx
@@ -106,6 +106,7 @@ const Header: React.FC<HeaderProps> = ({
   onSupportClick,
   isChatOpen = false,
   isSidebarOpen = false,
+  onToggleSidebar,
 }) => {
   const [logoHovered, setLogoHovered] = useState(false);
   const [dropdownOpen, setDropdownOpen] = useState(false);
